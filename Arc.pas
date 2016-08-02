28774e9e66dca437d23d1e9807dafb43e19b7daa
zensan <Zigmund.bulinsh@gmail.com>
2016-08-02 11:28:50 +0300

    Add files via upload

---
+++ Arc.pas
@@ -0,0 +1,84 @@
+unit uArc;
+
+interface
+
+type
+  Arc<T: class, constructor> = record
+  strict private
+    FValue: T;
+    FArcHolder: IInterface;
+    function GetValue: T;
+  private type
+    TArcHolder = class(TInterfacedObject)
+    private
+      FObject: TObject;
+    public
+      constructor Create(AObject: TObject);
+      destructor Destroy; override;
+    end;
+  public
+    constructor Create(AValue: T); overload;
+    class function Create: Arc<T>; overload; static;
+
+    class operator Implicit(AFrom: T): Arc<T>;
+    class operator Implicit(AFrom: Arc<T>): T;
+  end;
+
+implementation
+
+{ TArc<T> }
+
+constructor Arc<T>.Create(AValue: T);
+begin
+  try
+    FValue := AValue;
+    FArcHolder := TArcHolder.Create(AValue);
+  except
+    AValue.Free;
+    raise;
+  end;
+end;
+
+class function Arc<T>.Create: Arc<T>;
+begin
+  Result := Arc<T>.Create(T.Create);
+end;
+
+function Arc<T>.GetValue: T;
+var
+  F: Arc<T>;
+begin
+  if FArcHolder = nil then
+    begin
+      F := Arc<T>.Create(T.Create);
+      FValue := F.FValue;
+      FArcHolder := F.FArcHolder;
+    end;
+
+  Result := FValue;
+end;
+
+class operator Arc<T>.Implicit(AFrom: T): Arc<T>;
+begin
+  Result := Arc<T>.Create(AFrom);
+end;
+
+class operator Arc<T>.Implicit(AFrom: Arc<T>): T;
+begin
+  Result := AFrom.GetValue;
+end;
+
+{ TArc<T>.TArcHolder }
+
+constructor Arc<T>.TArcHolder.Create(AObject: TObject);
+begin
+  FObject := AObject;
+end;
+
+destructor Arc<T>.TArcHolder.Destroy;
+begin
+  FObject.Free;
+  inherited;
+end;
+
+end.
