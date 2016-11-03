a0af57350a9040da9f1d9a93e0ca69ee7b4e242a
zensan <Zigmund.bulinsh@gmail.com>
2016-11-03 18:36:16 +0200

    Add files via upload

---
+++ Demos/InterfacesNotImplemented/uTest.pas
@@ -0,0 +1,21 @@
+unit uTest;
+
+interface
+
+uses
+  System.SysUtils;
+
+type
+  ITest1 = interface
+    ['{3EEDAFD3-FD1A-436B-BADB-D34286CCBDD7}']
+    procedure Test1;
+  end;
+
+  ITest2 = interface
+    ['{3EEDAFD3-FD1A-436B-BADB-D34286CCBDD7}']
+    procedure Test2;
+  end;
+
+implementation
+
+end.
